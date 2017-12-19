a66a749836ce057466381db7d5b7445b7c63d1e2
Mohamed Imran <imranmohamed92612im@gmail.com>
2017-12-19 10:28:54 +0530

    one extra method is addes

--- Test.java
+++ Test.java
@@ -4,11 +4,13 @@ public static void main(String args[]){
 Scanner sc=new Scanner(System.in);
 System.out.println("Enter your rollno");
 int rollno=sc.nextInt();
+System.out.println("Enter your age");
+int age=sc.nextInt();
 System.out.println("Enter your name");
 String name=sc.next();
 System.out.println("Enter your fee");
 double fee=sc.nextDouble();
-System.out.println("Rollno:"+rollno+"name:"+name+"fee:"+fee);
+System.out.println("Rollno:"+rollno+"name:"+name+"fee:"+fee+"Age:"+age);
 sc.close();
 }
 }
